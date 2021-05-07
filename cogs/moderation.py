d0ed591958e0f5788c42c1289a7df693e8b1157e
FluxedScript <fluxedscript@gmail.com>
2021-05-07 23:30:15 +0100

    Efficiency update to moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -212,17 +212,22 @@ class Mod(commands.Cog):
             await ctx.send("Please provide a reason!")
             return
         time_warned = datetime.datetime.now()
-        async for x in posts.find({"guild_id": ctx.guild.id, "user_id": user.id}):
-            logs = x["mod_logs"]
+        data = await posts.find_one({"guild_id": ctx.guild.id, "user_id": user.id})
+        logs = data["mod_logs"]
 
         warn_count = len(logs) + 1
 
         warn_id = tools.generate_flake()
-        logs.append({"type": "WARNED", "warn_id": warn_id, "reason": reason, "issuer": ctx.author.id,
-                     "time": time_warned.strftime('%c')})
 
         await posts.update_one({"user_id": user.id, "guild_id": ctx.guild.id},
-                               {"$set": {"mod_logs": logs}})
+                               {"$push":
+                                   {"mod_logs": {
+                                       "type": "WARNED",
+                                       "warn_id": warn_id,
+                                       "reason": reason,
+                                       "issuer": ctx.author.id,
+                                       "time": time_warned.strftime('%c')
+                                   }}})
         embed = discord.Embed(colour=discord.Colour(0x36a39f), description=f"You have been warned!")
         embed.add_field(name="Reason:", value=f"{reason}", inline=False)
         embed.add_field(name="ID:", value=f"{warn_id}", inline=False)
@@ -268,7 +273,8 @@ class Mod(commands.Cog):
                 await user.send(embed=embed)
                 await ctx.send(f"{user.name}'s warning with the id of `{warn_id}` has been removed!")
             except:
-                await ctx.send(f"{user.name}'s warning with the id of `{warn_id}` has been removed but they did not recieve a dm!")
+                await ctx.send(
+                    f"{user.name}'s warning with the id of `{warn_id}` has been removed but they did not recieve a dm!")
             await ctx.message.delete()
 
             await posts.update_one({"guild_id": ctx.guild.id, "user_id": user.id},
