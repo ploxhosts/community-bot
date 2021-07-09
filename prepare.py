5e6870b358977deb17c1f8860fd40a0ae0a05a1b
FluxedScript <fluxedscript@gmail.com>
2021-07-09 21:18:20 +0100

    fix formatting

--- prepare.py
+++ prepare.py
@@ -1,14 +1,11 @@
 import os
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
+
 # init env #
 
 load_dotenv()
 
 connection_string = os.getenv("connection_string")
-
-
-
-
 
 database = AsyncIOMotorClient(connection_string)
