503da86f86d0ec545debb6c6c605c29dc52bc052
FluxedScript <fluxedscript@gmail.com>
2021-09-03 20:16:30 +0100

    fix error

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import discord from 'discord.js';
 const client = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
- 	console.log(`Logged in as ${client.user.tag}!`);
+ 	console.log(`Logged in as ${client.user?.username}!`);
 });
 
 client.on('interactionCreate', async interaction => {
