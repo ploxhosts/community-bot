322a1c73d9edcb87595846af5e0d7ad9c600504c
FluxedScript <fluxedscript@gmail.com>
2021-09-03 20:15:23 +0100

    add basic eslint

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import discord from 'discord.js';
+
+const client = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+ 	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+
+client.login('token');
