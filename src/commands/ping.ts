35beb3c3574a49805ddfa5763c3693b8cf63af89
FluxedScript <fluxedscript@gmail.com>
2021-09-04 10:05:16 +0100

    configurable ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,10 +1,15 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
+import discord from 'discord.js';
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
-	async execute(interaction: any) {
-		await interaction.reply('Pong!');
+	async execute(interaction: discord.CommandInteraction) {
+		const Embed = new discord.MessageEmbed()
+			.setColor(process.env.themeColor as discord.ColorResolvable)
+			.setDescription(`:hourglass: ${new Date().getTime() - interaction.createdTimestamp}ms\n\n:heartbeat: ${interaction.client.ws.ping} ms`)
+			.setFooter(`${process.env.brandName} - Commands`, interaction.client.user?.displayAvatarURL())
+		await interaction.reply({ embeds: [Embed] })
 	},
 };
