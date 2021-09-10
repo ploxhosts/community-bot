35f1ac430f81d917a891e64459a5be94a8c8dd69
FluxedScript <fluxedscript@gmail.com>
2021-09-10 14:16:42 +0100

    attach bad word check to message event

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,9 +1,11 @@
 import discord from 'discord.js';
+import checkForBadWords from './utils/checkForBadWords'
 
 module.exports = {
 	name: 'messageCreate',
 	dbRequired: true,
 	async execute(message: discord.Message) {
 		console.log("Message sent");
+		await checkForBadWords(message);
 	},
 };
--- src/events/utils/checkForBadWords.ts
+++ src/events/utils/checkForBadWords.ts
@@ -1,46 +1,47 @@
 import discord from 'discord.js';
 import connection from '../../db/mysql';
 
-module.exports = {
-	check(message: discord.Message) {
-		let badWords: string[] = [];
-		if (!message.guild) {
-			return false;
-		}
-		if (process.env.DB_MODE === "true") {
-			connection.getConnection(function(err, connection) {
-				if (err) {
-					console.log("Error connecting to database: " + err);
-					console.log("\x1b[31m"+ "Failed to connect to database!" + "\x1b[0m");
+
+async function check(message: discord.Message) {
+	let badWords: string[] = ["noob"];
+	if (!message.guild) {
+		return false;
+	}
+	if (process.env.DB_MODE === "true") {
+		connection.getConnection(function(err, connection) {
+			if (err) {
+				console.log("Error connecting to database: " + err);
+				console.log("\x1b[31m"+ "Failed to connect to database!" + "\x1b[0m");
 					
-					return null;
-				}
-				if (connection) {
-					connection.query("SELECT * FROM ploxy_bad_words WHERE guild_id = ?", String(message.guild?.id), function(err, rows) {
-						if (err) {
-							console.log("Error querying database: " + err);
-							console.log("\x1b[31m"+ "Failed to query database!" + "\x1b[0m");
+				return null;
+			}
+			if (connection) {
+				connection.query("SELECT * FROM ploxy_badwords WHERE guild_id = ?", String(message.guild?.id), function(err, rows) {
+					if (err) {
+						console.log("Error querying database: " + err);
+						console.log("\x1b[31m"+ "Failed to query database!" + "\x1b[0m");
+					}
+					if (rows) {
+						for (let i = 0; i < rows.length; i++) {
+							badWords.push(rows[i].word);
 						}
-						if (rows) {
-							for (let i = 0; i < rows.length; i++) {
-								badWords.push(rows[i].word);
-							}
-						}
-						connection.release();
-					});
-				}
-			});
-		}
-		if (badWords.length > 0) {
-			for (let i = 0; i < badWords.length; i++) {
-				if (message.content.toLowerCase().includes(badWords[i])) {
-					message.delete();
-					message.channel.send(`${message.author} you are not allowed to use that word!`);
-					return true;
-				}
+					}
+					connection.release();
+				});
+			}
+		});
+	}
+	if (badWords.length > 0) {
+			
+		for (let i = 0; i < badWords.length; i++) {
+			if (message.content.toLowerCase().includes(badWords[i])) {
+				message.delete();
+				message.channel.send(`${message.author} you are not allowed to use that word!`);
+				return true;
 			}
 		}
-		return false;
+	}
+	return false;
+}
 
-	},
-};
+export default check;
