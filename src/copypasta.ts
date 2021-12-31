3adaa2b690cbc7a630d0c7c3840ff7ade9f8f5b7
FluxedScript <fluxedscript@gmail.com>
2021-12-31 00:43:28 +0000

    finally fix files

--- src/copypasta.ts
+++ src/copypasta.ts
@@ -1,4 +1,4 @@
-const fakeDiscordAnnc =  [
+export const fakeDiscordAnnc =  [
   "out for a Discord", "going around", 
   "those who accept", "send this to as many", 
   "if you see this", "tell everyone on your friends list", 
@@ -9,7 +9,7 @@ const fakeDiscordAnnc =  [
   "please send", "send this message", "discord creator and he tells you", "you will not be deleted"
 ]
 
-const shareDeath = [
+export const shareDeath = [
   "once there was a little girl called", "by cutting you and watching you bleed to death",
   "send this to ten other", "either get kissed or asked out", "now every week on the day of her death",
   "creeps into your room and kills you slowly", "you will be dead", "funny you opened this because", 
@@ -17,13 +17,7 @@ const shareDeath = [
   "say about me", "ll have you know i", "i am trained", "not only am i extensively trained", "freaking dead"
 ]
 
-const unicode = [
+export const unicode = [
   "▀", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█", "▉", "▊", "▋", "▌", "▍", "▎", "▏", "▐", "▒", "░", "▓", "▔", "▕", "▖", "▗", "▘", "▙", "▚", "▛", "▜", "▝", "▞", "▟",
   "■", "□", "▢", "▣", "▤", "▥", "▦", "▧", "▨", "▩", "▪", "▫", "▬", "▭", "▮", "▯", "▰", "▱"
 ]
-
-module.exports = {
-  fakeDiscordAnnc,
-  shareDeath,
-  unicode
-}
--- src/utils/copyPastaCheck.ts
+++ src/utils/copyPastaCheck.ts
@@ -1,7 +1,12 @@
 import discord from 'discord.js';
 import connection from '../db/mysql';
-import copypasta from '../copypasta';
+import {fakeDiscordAnnc, unicode, shareDeath} from '../copypasta';
 
 async function copyPastaCheck(text: string, author: discord.User) {
-  copypasta.
+  let discordAnnouncementCount = 0;
+  let unicodeCount = 0;
+  let shareDeathCount = 0;
+
+  if (process.env.DB_MODE === "true") {
+  } // TODO: Use database to get the limits
 }
