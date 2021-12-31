52eb1d831c3f843a048f4e3d973088b2b262aca5
FluxedScript <fluxedscript@gmail.com>
2021-12-31 00:34:23 +0000

    change from json to ts

--- src/utils/copyPastaCheck.ts
+++ src/utils/copyPastaCheck.ts
@@ -1,13 +1,7 @@
 import discord from 'discord.js';
 import connection from '../db/mysql';
-import fs from 'fs';
-
-let copypasta = {}
-
-fs.readFile('../copypasta.txt', 'utf8', function(err, data) {
-  if (err) throw err;
-  copypasta = JSON.parse(data);
-});
+import copypasta from '../copypasta';
 
 async function copyPastaCheck(text: string, author: discord.User) {
+  copypasta.
 }
