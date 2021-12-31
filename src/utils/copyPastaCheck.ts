bc06b82e772408edd2d9cccd02a32ae737c3f94c
FluxedScript <fluxedscript@gmail.com>
2021-12-31 00:48:26 +0000

    get rid of author and discord things from copyPastaCheck

--- src/utils/copyPastaCheck.ts
+++ src/utils/copyPastaCheck.ts
@@ -1,8 +1,6 @@
-import discord from 'discord.js';
-import connection from '../db/mysql';
 import {fakeDiscordAnnc, unicode, shareDeath} from '../copypasta';
 
-async function copyPastaCheck(text: string, author: discord.User) {
+async function copyPastaCheck(text: string) {
   let discordAnnouncementCount = 0;
   let unicodeCount = 0;
   let shareDeathCount = 0;
