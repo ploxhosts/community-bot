7e26b06330aa3e0793ba32e0b4387351f4534e7a
FluxedScript <fluxedscript@gmail.com>
2022-01-09 11:02:54 +0000

    update spacing and add UpdateUser

--- src/db/services/User.ts
+++ src/db/services/User.ts
@@ -34,29 +34,57 @@ export const getUser = async (
     discriminator?: string,
     email?: string
   ) => {
-    let query = "SELECT * FROM ploxy_users WHERE ";
-    let values = [];
-    if (user_id) {
-      query += "user_id = $1";
-      values.push(user_id);
-    }
-    if ((username && discriminator) && !user_id) {
-      query += "username = $2 && discriminator = $3";
-      values.push(username, discriminator);
-    }
-    if (email && !(user_id && username && discriminator)) {
-      query += "email = $4";
-      values.push(email);
-    }
-    if (values.length === 0) {
-      log.debug("getUser failed, no values in function to search for");
-      return false;
-    }
-
-    const res = await postgres.query(query, values);
-    if (res.rows.length > 0) {
-      return res.rows[0];
-    }
-    log.debug(`User ${user_id} not found, getUser failed`);
+  
+  let query = "SELECT * FROM ploxy_users WHERE ";
+  let values = [];
+  if (user_id) {
+    query += "user_id = $1";
+    values.push(user_id);
+  }
+  if ((username && discriminator) && !user_id) {
+    query += "username = $2 && discriminator = $3";
+    values.push(username, discriminator);
+  }
+  if (email && !(user_id && username && discriminator)) {
+     query += "email = $4";
+    values.push(email);
+  }
+  if (values.length === 0) {
+    log.debug("getUser failed, no values in function to search for");
     return false;
+  }
+
+  const res = await postgres.query(query, values);
+  if (res.rows.length > 0) {
+    return res.rows[0];
+  }
+  log.debug(`User ${user_id} not found, getUser failed`);
+  return false;
+}
+
+export const updateUser = async (
+    user_id: string,
+    username: string,
+    discriminator: string,
+    user_avatar: string | null = null,
+    email: string | null = null,
+    premium : number,
+    banned: number = 0
+  ) => {
+  const res = await getUser(user_id);
+  if (res.rows.length === 0) {
+    log.debug(`User ${user_id} not found, updateUser failed`);
+    return false;
+  }
+  let result;
+  try {
+    result = await postgres.query(
+      "UPDATE ploxy_users SET username = $1, discriminator = $2, user_avatar = $3, email = $4, premium = $5, banned = $6 WHERE user_id = $7 RETURNING *",
+      [username, discriminator, user_avatar, email, premium, banned, user_id])
+  } catch (err: any) {
+    log.error(err);
+    return false;
+  }
+  
+  return result.rows[0];
 }
