5d0cdc5b9098a9326c1f8756dba5962a86c21ecc
FluxedScript <fluxedscript@gmail.com>
2022-01-19 09:53:23 +0000

    update spam check

--- src/utils/spamCheck.ts
+++ src/utils/spamCheck.ts
@@ -8,10 +8,6 @@ export const spamCheck = async (text: string, author_message_count: number, risk
   }
 
   const links = await getLinks(text)
-  if (links.keys.length > 0) {
-    riskScore += 3
-    console.log("Add risk score by a link existing", links, riskScore)
-  }
 
   for (const link of links) {
     let result = await checkLink(link);
@@ -23,16 +19,16 @@ export const spamCheck = async (text: string, author_message_count: number, risk
 
   if (author_message_count <= 3){
     riskScore += 5;
-    console.log("Add risk score by author_message_count 1", author_message_count, riskScore)
+    console.log("Add risk score by author_message_count 1", "author_message_count: " + author_message_count , riskScore)
   } else if (author_message_count <= 10){
     riskScore += 4;
-    console.log("Add risk score by author_message_count 2", author_message_count, riskScore)
+    console.log("Add risk score by author_message_count 2", "author_message_count: " + author_message_count, riskScore)
   } else if (author_message_count <= 30){
     riskScore += 3;
-    console.log("Add risk score by author_message_count 3", author_message_count, riskScore)
+    console.log("Add risk score by author_message_count 3", "author_message_count: " + author_message_count, riskScore)
   } else if (author_message_count <= 40){
     riskScore += 2;
-    console.log("Add risk score by author_message_count 4", author_message_count, riskScore)
+    console.log("Add risk score by author_message_count 4", "author_message_count: " + author_message_count, riskScore)
   }
 
   let badwordsScore = await badWordCheck(text, true) // Check for bad words even if disabled to check if it's spam - does not moderate bad words
@@ -41,12 +37,16 @@ export const spamCheck = async (text: string, author_message_count: number, risk
   
   const mediumRiskWords = ["join", "now", "quick", "running out", "offer", "nitro", "$", "gain", "profit", "free"]
   const highRiskWords = ["!!", "gift", "nitro", "steam", "before the action"]
-  for (const word of text.toLowerCase().split(" ")){
-    if (mediumRiskWords.includes(word)){
-      console.log("add risk by mediumRiskWords", word, riskScore)
+  for (const mediumWord of mediumRiskWords) {
+    if (text.toLowerCase().includes(mediumWord)) {
+      console.log("Add risk by mediumRiskWords", mediumWord, riskScore)
       riskScore += 2
-    } else if (highRiskWords.includes(word)){
-      console.log("add risk by highRiskWords", word, riskScore)
+    }
+  }
+
+  for (const highWord of highRiskWords){
+    if (text.toLowerCase().includes(highWord)){
+      console.log("Aadd risk by highRiskWords", highWord, riskScore)
       riskScore += 3
     }
   }
