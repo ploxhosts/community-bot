520970c844d3e4de7fa1196030db308eae734cd2
FluxedScript <48490268+athyk@users.noreply.github.com>
2022-02-20 10:28:34 +0000

    update DAO

--- src/db/services/Links.ts
+++ src/db/services/Links.ts
@@ -6,27 +6,32 @@ import postgres from '../postgres';
 let redis: RedisClientType;
 
 /**
- * Adds a bad link
+ * Adds a link
  *
- * @param badLink- The bad link to be stored Example https://example.com
+ * @param link- The link to be stored Example https://example.com
  * @param addedBy - The user id Example 1232132132131231
  * @param score - The threat level
+ * @param process - the logs of what happened to gain score
+ * @param guild_id - The guild id that set the link or set undefined if done by system
+ *
  *
  */
-export const addBadLink = async (
-    badLink: string,
+export const addLink = async (
+    link: string,
     addedBy: string | undefined,
     score: number,
-    process: { type: string; score: number }[]
+    process: { type: string; score: number }[],
+    guildId: string | undefined,
+    allowed: boolean
 ) => {
     try {
         const result = await postgres.query(
-            'INSERT INTO ploxy_badlinks (link, added_by, score, process) VALUES ($1, $2, $3)',
-            [badLink, addedBy, score, process.toString()]
+            'INSERT INTO ploxy_links (link, added_by, score, process, guild_id, allowed) VALUES ($1, $2, $3, $4, $5)',
+            [link, addedBy, score, process.toString(), guildId, allowed]
         );
 
         await redis.set(
-            `badLink:${badLink}`,
+            `link:${link}:${guildId}`,
             JSON.stringify(result.rows.at(0))
         );
     } catch (error: any) {
@@ -37,19 +42,20 @@ export const addBadLink = async (
 };
 
 /**
- * Removes a bad link
+ * Removes a link
  *
- * @param badLink - The bad link to be stored Example https://example.com
- *
+ * @param link - The link to be stored Example https://example.com
+ * @param guildId - The guild id that set the link or set undefined if done by system
  */
 
-export const removeBadLink = async (badLink: string) => {
+export const removeLink = async (link: string, guildId: string | undefined) => {
     try {
-        await postgres.query('DELETE FROM ploxy_badlinks WHERE link = $1', [
-            badLink,
-        ]);
+        await postgres.query(
+            'DELETE FROM ploxy_links WHERE link = $1 AND guild_id = $2',
+            [link, guildId]
+        );
 
-        return await redis.del(`badLink:${badLink}`);
+        return await redis.del(`link:${link}:${guildId}`);
     } catch (error: any) {
         log.error(error);
 
@@ -62,11 +68,20 @@ export const removeBadLink = async (badLink: string) => {
  *
  */
 
-export const checkBadLink = async (link: string): Promise<any> => {
+export const checkLink = async (
+    link: string,
+    guildId: string | undefined
+): Promise<any> => {
     try {
+        const redisResult = await redis.get(`link:${link}:${guildId}`);
+
+        if (redisResult) {
+            return JSON.parse(redisResult);
+        }
+
         const result = await postgres.query(
-            'SELECT * FROM ploxy_badlinks WHERE link = $1',
-            [link]
+            'SELECT * FROM ploxy_links WHERE link = $1 AND guild_id = $2',
+            [link, guildId]
         );
 
         if (result.rowCount > 0) {
@@ -82,13 +97,32 @@ export const checkBadLink = async (link: string): Promise<any> => {
 };
 
 /**
- * Gets all badlinks
+ * Gets all links
  *
  */
 
-export const getAllBadLinks = async () => {
+export const getAllLinks = async () => {
     try {
-        return await postgres.query('SELECT * FROM ploxy_badlinks');
+        return await postgres.query('SELECT * FROM ploxy_links');
+    } catch (error: any) {
+        log.error(error);
+
+        return false;
+    }
+};
+
+/**
+ * Gets all links based on guildId
+ *
+ * @param guildId - The guild id that set the link or set undefined if done by system
+ */
+
+export const getAllLinksByGuildId = async (guildId: string) => {
+    try {
+        return await postgres.query(
+            'SELECT * FROM ploxy_links WHERE guild_id = $1',
+            [guildId]
+        );
     } catch (error: any) {
         log.error(error);
 
@@ -100,7 +134,8 @@ module.exports = {
     setRedis: function (redisIn: RedisClientType) {
         redis = redisIn;
     },
-    getAllBadLinks,
-    removeBadLink,
-    addBadLink,
+    getAllLinks,
+    removeLink,
+    addLink,
+    getAllLinksByGuildId,
 };
