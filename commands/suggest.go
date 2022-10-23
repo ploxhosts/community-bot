5798ef3a6ce550de2e398dd06b132f8b6c3dd977
athyk <48490268+athyk@users.noreply.github.com>
2022-10-23 01:36:49 +0100

    follow dry

--- commands/suggest.go
+++ commands/suggest.go
@@ -20,6 +20,25 @@ type Suggestion struct {
 	Voters     []string `json:"voters"`
 }
 
+func handleError(client *discordgo.Session, interaction *discordgo.InteractionCreate) {
+	embed := &discordgo.MessageEmbed{
+		Color:       0x36a39f,
+		Description: "There was an error sending your suggestion. Please try again later.",
+		Footer: &discordgo.MessageEmbedFooter{
+			Text: "Ploxy | Suggestions",
+		},
+	}
+
+	err := client.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{embed},
+		},
+	})
+	if err != nil {
+		return
+	}
+}
 func SuggestCommand(client *discordgo.Session, interaction *discordgo.InteractionCreate) {
 	// Get options
 	options := interaction.ApplicationCommandData().Options
@@ -45,98 +64,38 @@ func SuggestCommand(client *discordgo.Session, interaction *discordgo.Interactio
 	channel, err := client.Channel(utils.Config.SuggestionChannelId)
 
 	if err != nil {
-		embed := &discordgo.MessageEmbed{
-			Color:       0x36a39f,
-			Description: "There was an error sending your suggestion. Please try again later.",
-			Footer: &discordgo.MessageEmbedFooter{
-				Text: "Ploxy | Suggestions",
-			},
-		}
-
-		err = client.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Embeds: []*discordgo.MessageEmbed{embed},
-			},
-		})
-		if err != nil {
-			return
-		}
+		handleError(client, interaction)
 		return
 	}
 
 	message, err := client.ChannelMessageSendEmbed(channel.ID, embed)
 	if err != nil {
-
-		err := client.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "There was an error saving your suggestion. Please try again later.",
-			},
-		})
-		if err != nil {
-			return
-		}
+		handleError(client, interaction)
 		return
 	}
 
 	err = client.MessageReactionAdd(message.ChannelID, message.ID, "✅")
 	if err != nil {
-
-		err := client.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "There was an error saving your suggestion. Please try again later.",
-			},
-		})
-		if err != nil {
-			return
-		}
+		handleError(client, interaction)
 		return
 	}
 
 	err = client.MessageReactionAdd(message.ChannelID, message.ID, "🟧")
 	if err != nil {
-
-		err := client.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "There was an error saving your suggestion. Please try again later.",
-			},
-		})
-		if err != nil {
-			return
-		}
+		handleError(client, interaction)
 		return
 	}
 
 	err = client.MessageReactionAdd(message.ChannelID, message.ID, "❌")
 	if err != nil {
-
-		err := client.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "There was an error saving your suggestion. Please try again later.",
-			},
-		})
-		if err != nil {
-			return
-		}
+		handleError(client, interaction)
 		return
 	}
 
 	var suggestions []Suggestion
 	fileErr := utils.ReadJsonFile("suggestions.json", &suggestions)
 	if fileErr != nil {
-		err := client.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "There was an error saving your suggestion. Please try again later.",
-			},
-		})
-		if err != nil {
-			return
-		}
+		handleError(client, interaction)
 		return
 	}
 
@@ -155,15 +114,7 @@ func SuggestCommand(client *discordgo.Session, interaction *discordgo.Interactio
 
 	fileErr = utils.WriteToJsonFile("suggestions.json", suggestions)
 	if fileErr != nil {
-		err := client.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "There was an error saving your suggestion. Please try again later.",
-			},
-		})
-		if err != nil {
-			return
-		}
+		handleError(client, interaction)
 		return
 	}
 
--- commands/suggestions.go
+++ commands/suggestions.go
@@ -1,1 +1,15 @@
 package commands
+
+import "github.com/bwmarrin/discordgo"
+
+func SuggestionsCommand(client *discordgo.Session, interaction *discordgo.InteractionCreate) {
+	// get the subcommand
+	subcommand := interaction.ApplicationCommandData().Options[0].Name
+
+	// get the suggestion ID
+	suggestionId := interaction.ApplicationCommandData().Options[0].Options[0].StringValue()
+
+	// get the reason
+	reason := interaction.ApplicationCommandData().Options[0].Options[1].StringValue()
+	
+}
